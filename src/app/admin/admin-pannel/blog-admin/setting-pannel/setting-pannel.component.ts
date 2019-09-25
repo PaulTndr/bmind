cb89cb65ab5ce1db64ab7c698cc9ce53ccf9d9d8
Paul TONDEREAU <paul.tondereau@soprasteria.com>
2019-09-25 10:10:52 +0200

    Ajout des offres en admin

--- src/app/admin/admin-pannel/blog-admin/setting-pannel/setting-pannel.component.ts
+++ src/app/admin/admin-pannel/blog-admin/setting-pannel/setting-pannel.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+import { MatTableDataSource } from '@angular/material';
 
-import {Keyword} from '../../../../classes/articles/keyword'
-import {Auteur} from '../../../../classes/articles/auteur'
+import { Keyword } from '../../../../classes/articles/keyword'
+import { Auteur } from '../../../../classes/articles/auteur'
 
 import { HttpClient } from '@angular/common/http';
+import { Offre } from 'src/app/classes/offre';
 
 @Component({
   selector: 'app-setting-pannel',
@@ -13,35 +14,39 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SettingPannelComponent implements OnInit {
 
-  entryToEdit : String;
+  entryToEdit: String;
 
-  filterText : String;
+  filterText: String;
 
-  displayedColumnsKeyword : String[] = ['key', 'def', 'actions'];
-  listKeyword : Keyword[] = [];
+  displayedColumnsKeyword: String[] = ['key', 'def', 'actions'];
+  listKeyword: Keyword[] = [];
   dataSourceKeyword = new MatTableDataSource<Keyword>();
 
-  displayedColumnsAutor : String[] = ['prenom', 'nom', 'fonction', 'actions'];
-  listAutor : Auteur[] = [];
+  displayedColumnsAutor: String[] = ['prenom', 'nom', 'fonction', 'actions'];
+  listAutor: Auteur[] = [];
   dataSourceAutor = new MatTableDataSource<Auteur>();
 
-  displayedColumnsSecteurs : String[] = ['key', 'actions'];
-  listSecteurs : String[] = [];
+  displayedColumnsSecteurs: String[] = ['key', 'actions'];
+  listSecteurs: String[] = [];
   dataSourceSecteurs = new MatTableDataSource<String>();
 
-  displayedColumnsTypes : String[] = ['key', 'actions'];
-  listTypes : String[] = [];
+  displayedColumnsTypes: String[] = ['key', 'actions'];
+  listTypes: String[] = [];
   dataSourceTypes = new MatTableDataSource<String>();
 
-  isDeletePopup : boolean = false;
-  isAddOrEditPopup : boolean = false;
+  displayedColumnsOffers: String[] = ['title', 'linkPDF', 'actions'];
+  listOffers: Offre[] = [];
+  dataSourceOffers = new MatTableDataSource<Offre>();
 
-  newKeyword : Keyword = new Keyword();
-  lastIdKw : Number;
-  newAutor : Auteur = new Auteur();
-  lastIdAutor : Number;
+  isDeletePopup: boolean = false;
+  isAddOrEditPopup: boolean = false;
 
-  mode : String;
+  newKeyword: Keyword = new Keyword();
+  lastIdKw: Number;
+  newAutor: Auteur = new Auteur();
+  lastIdAutor: Number;
+
+  mode: String;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -54,20 +59,20 @@ export class SettingPannelComponent implements OnInit {
     this.refreshData()
   }
 
-  fillDataFromBase(){
+  fillDataFromBase() {
     this.httpClient.get<any[]>('https://bminddev.firebaseio.com/keywords.json').subscribe(
       (response) => {
         var lKeys = Object.keys(response)
-        var listObject : Keyword[] = [];
-        lKeys.forEach(function(kw){
+        var listObject: Keyword[] = [];
+        lKeys.forEach(function (kw) {
           var oneKw = new Keyword()
           oneKw.fromHashMap(response[kw])
           listObject.push(oneKw)
         })
         this.listKeyword = listObject.slice();
         this.lastIdKw = 0;
-        for (var k=0; k<this.listKeyword.length; k++){
-          this.lastIdKw=+Math.max(Number(this.lastIdKw),Number(this.listKeyword[k].id))
+        for (var k = 0; k < this.listKeyword.length; k++) {
+          this.lastIdKw = +Math.max(Number(this.lastIdKw), Number(this.listKeyword[k].id))
         }
         this.refreshData()
       },
@@ -79,16 +84,16 @@ export class SettingPannelComponent implements OnInit {
     this.httpClient.get<any[]>('https://bminddev.firebaseio.com/autors.json').subscribe(
       (response) => {
         var lKeys = Object.keys(response)
-        var listObject : Auteur[] = [];
-        lKeys.forEach(function(kw){
+        var listObject: Auteur[] = [];
+        lKeys.forEach(function (kw) {
           var oneAutor = new Auteur()
           oneAutor.fromHashMap(response[kw])
           listObject.push(oneAutor)
         })
         this.listAutor = listObject.slice();
         this.lastIdAutor = 0;
-        for (var k=0; k<this.listAutor.length; k++){
-          this.lastIdAutor=+Math.max(Number(this.lastIdAutor),Number(this.listAutor[k].id))
+        for (var k = 0; k < this.listAutor.length; k++) {
+          this.lastIdAutor = +Math.max(Number(this.lastIdAutor), Number(this.listAutor[k].id))
         }
       },
       (error) => {
@@ -113,24 +118,65 @@ export class SettingPannelComponent implements OnInit {
         console.log('Erreur ! : ' + error);
       }
     );
-    
+
+    var offer1 = new Offre();
+    offer1.title = "Chargé.e de communication"
+    offer1.linkPDF = "http://ptondereau.perso.centrale-marseille.fr/assets/pdfOffres/offre1.pdf"
+    var offer2 = new Offre();
+    offer2.title = "Développeur web"
+    offer2.linkPDF = "http://ptondereau.perso.centrale-marseille.fr/assets/pdfOffres/offre2.pdf"
+    var offer3 = new Offre();
+    offer3.title = "Traducteur anglais/français"
+    offer3.linkPDF = "http://ptondereau.perso.centrale-marseille.fr/assets/pdfOffres/offre3.pdf"
+    var listInit = [];
+    listInit.push(offer1);
+    listInit.push(offer2);
+    listInit.push(offer3);
+
+    this.httpClient.put('https://bminddev.firebaseio.com/offers.json', listInit).subscribe(
+      () => {
+        console.log('Edition du mot clé terminée !');
+      },
+      (error) => {
+        console.log('Erreur lors de l\'edition du mot clé! : ' + error);
+      }
+    );
+
+    this.httpClient.get<any[]>('https://bminddev.firebaseio.com/offers.json').subscribe(
+      (response) => {
+        var lKeys = Object.keys(response)
+        var listObject: Offre[] = [];
+        lKeys.forEach(function (kw) {
+          var oneOffre = new Offre()
+          oneOffre.fromHashMap(response[kw])
+          listObject.push(oneOffre)
+        })
+        this.listOffers = listObject.slice();
+        this.lastIdKw = 0;
+        this.refreshData()
+      },
+      (error) => {
+        console.log('Erreur ! : ' + error);
+      }
+    );
+
   }
 
 
-  filter(){
-    if(this.entryToEdit==='keyword'){
-      var listKwToDisplay : Keyword[] = [];
-      for (var k=0; k<this.listKeyword.length; k++){
-        if(this.listKeyword[k].key.toLowerCase().startsWith(this.filterText.toLowerCase())){
+  filter() {
+    if (this.entryToEdit === 'keyword') {
+      var listKwToDisplay: Keyword[] = [];
+      for (var k = 0; k < this.listKeyword.length; k++) {
+        if (this.listKeyword[k].key.toLowerCase().startsWith(this.filterText.toLowerCase())) {
           listKwToDisplay.push(this.listKeyword[k])
         }
       }
 
       this.dataSourceKeyword = new MatTableDataSource<Keyword>(listKwToDisplay);
-    } else if(this.entryToEdit==='autor'){
-      var listAutorToDisplay : Auteur[] = [];
-      for (var k=0; k<this.listAutor.length; k++){
-        if(this.listAutor[k].prenom.toLowerCase().startsWith(this.filterText.toLowerCase())){
+    } else if (this.entryToEdit === 'autor') {
+      var listAutorToDisplay: Auteur[] = [];
+      for (var k = 0; k < this.listAutor.length; k++) {
+        if (this.listAutor[k].prenom.toLowerCase().startsWith(this.filterText.toLowerCase())) {
           listAutorToDisplay.push(this.listAutor[k])
         }
       }
@@ -140,67 +186,67 @@ export class SettingPannelComponent implements OnInit {
 
   }
 
-  openPopup(){
-    if (this.mode==="Suppression"){
-      this.isDeletePopup=true;
+  openPopup() {
+    if (this.mode === "Suppression") {
+      this.isDeletePopup = true;
     } else {
-      this.isAddOrEditPopup=true;
+      this.isAddOrEditPopup = true;
     }
   }
 
-  closePopup(){
-    this.isDeletePopup=false;
-    this.isAddOrEditPopup=false;
+  closePopup() {
+    this.isDeletePopup = false;
+    this.isAddOrEditPopup = false;
     this.newKeyword = new Keyword();
     this.newAutor = new Auteur();
   }
 
-  addItem(){
-    this.mode="Ajout"
+  addItem() {
+    this.mode = "Ajout"
     this.openPopup();
   }
 
-  editItem(idItem : Number){
-    this.mode="Édition"
-    if(this.entryToEdit==='keyword'){
-      for (var k=0; k<this.listKeyword.length; k++){
-        if(this.listKeyword[k].id===idItem){
-          this.newKeyword=this.listKeyword[k].copy()
+  editItem(idItem: Number) {
+    this.mode = "Édition"
+    if (this.entryToEdit === 'keyword') {
+      for (var k = 0; k < this.listKeyword.length; k++) {
+        if (this.listKeyword[k].id === idItem) {
+          this.newKeyword = this.listKeyword[k].copy()
         }
       }
-    } else if(this.entryToEdit==='autor'){
-      for (var k=0; k<this.listAutor.length; k++){
-        if(this.listAutor[k].id===idItem){
-          this.newAutor=this.listAutor[k].copy()
+    } else if (this.entryToEdit === 'autor') {
+      for (var k = 0; k < this.listAutor.length; k++) {
+        if (this.listAutor[k].id === idItem) {
+          this.newAutor = this.listAutor[k].copy()
         }
       }
     }
     this.openPopup();
   }
 
-  deleteItem(idItem : Number){
-    this.mode="Suppression";
-    if(this.entryToEdit==='keyword'){
-      for (var k=0; k<this.listKeyword.length; k++){
-        if(this.listKeyword[k].id===idItem){
-          this.newKeyword=this.listKeyword[k].copy()
+  deleteItem(idItem: Number) {
+    this.mode = "Suppression";
+    if (this.entryToEdit === 'keyword') {
+      for (var k = 0; k < this.listKeyword.length; k++) {
+        if (this.listKeyword[k].id === idItem) {
+          this.newKeyword = this.listKeyword[k].copy()
         }
       }
-    } else if(this.entryToEdit==='autor'){
-      for (var k=0; k<this.listAutor.length; k++){
-        if(this.listAutor[k].id===idItem){
-          this.newAutor=this.listAutor[k].copy()
+    } else if (this.entryToEdit === 'autor') {
+      for (var k = 0; k < this.listAutor.length; k++) {
+        if (this.listAutor[k].id === idItem) {
+          this.newAutor = this.listAutor[k].copy()
         }
       }
     }
     this.openPopup();
   }
 
-  validateAction(){
-    if(this.entryToEdit==='keyword'){
-      if(this.mode==="Ajout"){
-        this.newKeyword.id = (+this.lastIdKw)+1;
-        this.lastIdKw=(+this.lastIdKw)+1;
+  validateAction() {
+    if (this.entryToEdit === 'keyword') {
+      if (this.mode === "Ajout") {
+        this.newKeyword.id = (+this.lastIdKw) + 1;
+        this.lastIdKw = (+this.lastIdKw) + 1;
 
         this.listKeyword.push(this.newKeyword)
 
@@ -214,11 +260,11 @@ export class SettingPannelComponent implements OnInit {
           }
         );
 
-      } else if(this.mode==="Édition"){
-        for (var k=0; k<this.listKeyword.length; k++){
-          if(this.listKeyword[k].id===this.newKeyword.id){
-            this.listKeyword[k].key=this.newKeyword.key;
-            this.listKeyword[k].def=this.newKeyword.def;
+      } else if (this.mode === "Édition") {
+        for (var k = 0; k < this.listKeyword.length; k++) {
+          if (this.listKeyword[k].id === this.newKeyword.id) {
+            this.listKeyword[k].key = this.newKeyword.key;
+            this.listKeyword[k].def = this.newKeyword.def;
           }
         }
         this.httpClient.put('https://bminddev.firebaseio.com/keywords.json', this.listKeyword).subscribe(
@@ -229,14 +275,14 @@ export class SettingPannelComponent implements OnInit {
             console.log('Erreur lors de l\'edition du mot clé! : ' + error);
           }
         );
-      } else if(this.mode==="Suppression"){
-        var newListKw : Keyword[] = [];
-        for (var k=0; k<this.listKeyword.length; k++){
-          if(this.listKeyword[k].id!==this.newKeyword.id){
+      } else if (this.mode === "Suppression") {
+        var newListKw: Keyword[] = [];
+        for (var k = 0; k < this.listKeyword.length; k++) {
+          if (this.listKeyword[k].id !== this.newKeyword.id) {
             newListKw.push(this.listKeyword[k])
           }
         }
-        this.listKeyword=newListKw.slice();
+        this.listKeyword = newListKw.slice();
         this.httpClient.put('https://bminddev.firebaseio.com/keywords.json', this.listKeyword).subscribe(
           () => {
             console.log('Suppression du mot clé terminée !');
@@ -246,11 +292,11 @@ export class SettingPannelComponent implements OnInit {
           }
         );
       }
-    } else if(this.entryToEdit==='autor'){
-      if(this.mode==="Ajout"){
+    } else if (this.entryToEdit === 'autor') {
+      if (this.mode === "Ajout") {
 
-        this.newAutor.id = (+this.lastIdAutor)+1;
-        this.lastIdAutor=(+this.lastIdAutor)+1;
+        this.newAutor.id = (+this.lastIdAutor) + 1;
+        this.lastIdAutor = (+this.lastIdAutor) + 1;
 
         this.listAutor.push(this.newAutor)
 
@@ -263,14 +309,14 @@ export class SettingPannelComponent implements OnInit {
             console.log('Erreur lors de l\'enregistrment de l\'auteur! : ' + error);
           }
         );
-        
-        
-      } else if(this.mode==="Édition"){
-        for (var k=0; k<this.listAutor.length; k++){
-          if(this.listAutor[k].id===this.newAutor.id){
-            this.listAutor[k].prenom=this.newAutor.prenom;
-            this.listAutor[k].nom=this.newAutor.nom;
-            this.listAutor[k].fonction=this.newAutor.fonction;
+
+
+      } else if (this.mode === "Édition") {
+        for (var k = 0; k < this.listAutor.length; k++) {
+          if (this.listAutor[k].id === this.newAutor.id) {
+            this.listAutor[k].prenom = this.newAutor.prenom;
+            this.listAutor[k].nom = this.newAutor.nom;
+            this.listAutor[k].fonction = this.newAutor.fonction;
           }
         }
         this.httpClient.put('https://bminddev.firebaseio.com/autors.json', this.listAutor).subscribe(
@@ -281,14 +327,14 @@ export class SettingPannelComponent implements OnInit {
             console.log('Erreur lors de l\'edition de l\'auteur! : ' + error);
           }
         );
-      } else if(this.mode==="Suppression"){
-        var newListAutor : Auteur[] = [];
-        for (var k=0; k<this.listAutor.length; k++){
-          if(this.listAutor[k].id!==this.newAutor.id){
+      } else if (this.mode === "Suppression") {
+        var newListAutor: Auteur[] = [];
+        for (var k = 0; k < this.listAutor.length; k++) {
+          if (this.listAutor[k].id !== this.newAutor.id) {
             newListAutor.push(this.listAutor[k])
           }
         }
-        this.listAutor=newListAutor.slice();
+        this.listAutor = newListAutor.slice();
         this.httpClient.put('https://bminddev.firebaseio.com/autors.json', this.listAutor).subscribe(
           () => {
             console.log('Suppression de l\'auteur terminée !');
@@ -300,32 +346,34 @@ export class SettingPannelComponent implements OnInit {
       }
     }
 
-    this.refreshData(); 
+    this.refreshData();
     this.closePopup();
     this.filter();
   }
 
-  refreshData(){
-    this.filterText="";
-    if(this.entryToEdit==='keyword'){
+  refreshData() {
+    this.filterText = "";
+    if (this.entryToEdit === 'keyword') {
       this.dataSourceKeyword = new MatTableDataSource<Keyword>(this.listKeyword);
-    } else if(this.entryToEdit==='autor'){
+    } else if (this.entryToEdit === 'autor') {
       this.dataSourceAutor = new MatTableDataSource<Auteur>(this.listAutor);
-    } else if(this.entryToEdit==='secteurAndType'){
+    } else if (this.entryToEdit === 'secteurAndType') {
       this.dataSourceSecteurs = new MatTableDataSource<String>(this.listSecteurs);
       this.dataSourceTypes = new MatTableDataSource<String>(this.listTypes);
+    } else if (this.entryToEdit === 'offerCarr') {
+      this.dataSourceOffers = new MatTableDataSource<Offre>(this.listOffers);
     }
   }
 
-  deleteItemDirect(keyType : String, element : String){
-    if (keyType==='sector'){
-      var newListSecteurs=[]
-      for (var k=0; k<this.listSecteurs.length;k++){
-        if (this.listSecteurs[k]!=element){
+  deleteItemDirect(keyType: String, element: String) {
+    if (keyType === 'sector') {
+      var newListSecteurs = []
+      for (var k = 0; k < this.listSecteurs.length; k++) {
+        if (this.listSecteurs[k] != element) {
           newListSecteurs.push(this.listSecteurs[k])
         }
       }
-      this.listSecteurs=newListSecteurs.slice();
+      this.listSecteurs = newListSecteurs.slice();
       //Changement base
       this.httpClient.put('https://bminddev.firebaseio.com/secteurs.json', this.listSecteurs).subscribe(
         () => {
@@ -335,14 +383,14 @@ export class SettingPannelComponent implements OnInit {
           console.log('Erreur lors de la suppression du mot secteur! : ' + error);
         }
       );
-    } else if (keyType==='type'){
-      var newListTypes=[]
-      for (var k=0; k<this.listTypes.length;k++){
-        if (this.listTypes[k]!=element){
+    } else if (keyType === 'type') {
+      var newListTypes = []
+      for (var k = 0; k < this.listTypes.length; k++) {
+        if (this.listTypes[k] != element) {
           newListTypes.push(this.listTypes[k])
         }
       }
-      this.listTypes=newListTypes.slice();
+      this.listTypes = newListTypes.slice();
       //Changement base
       this.httpClient.put('https://bminddev.firebaseio.com/types.json', this.listTypes).subscribe(
         () => {
--- src/app/classes/offre.ts
+++ src/app/classes/offre.ts
@@ -1,4 +1,14 @@
+interface OfferData {
+    title: String
+    linkPDF: String
+}
+
 export class Offre {
     title: String;
     linkPDF: String;
+
+    fromHashMap(data: OfferData) {
+        this.title = String(data.title);
+        this.linkPDF = String(data.linkPDF);
+    }
 }
