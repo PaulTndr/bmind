3188f37e6cb802d6d49558f4f1f9a553627f7b2b
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-09-27 11:59:12 +0200

    Blog responsive

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -11,7 +11,7 @@ import { Article } from '../classes/articles/article'
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss', './blog.component.responsive.scss']
 })
 export class BlogComponent implements OnInit {
 
