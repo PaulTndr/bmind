2a5bad8edff7868c1fb149f2229416fca64cf86d
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-10-15 20:52:51 +0200

    Correction langue miniature

--- src/app/objects/miniature-article/miniature-article.component.ts
+++ src/app/objects/miniature-article/miniature-article.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges, Input,SimpleChanges } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 import { Article } from '../../classes/articles/article'
 @Component({
@@ -12,10 +13,14 @@ export class MiniatureArticleComponent implements OnInit, OnChanges {
   @Input() article : Article;
   styleForBk : SafeStyle;
 
-  constructor(private sanitizer : DomSanitizer) { }
+  constructor(private sanitizer : DomSanitizer, private translate: TranslateService) { }
 
   ngOnInit() {
     this.styleForBk = this.sanitizer.bypassSecurityTrustStyle("background-image: url("+this.article.srcImg+");");
+    console.log(this.article.time)
+    if (this.article.langue==='EN' && this.article.time==="1 heure"){
+      this.article.time="1 hour"
+    }
   }
 
   ngOnChanges(changes: SimpleChanges){
