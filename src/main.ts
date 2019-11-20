766f757eedccf48d119bab822bb341a572603b57
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-11-20 21:19:53 +0100

    prod mode

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import './polyfills';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+  if(window){
+    window.console.log=function(){};
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
