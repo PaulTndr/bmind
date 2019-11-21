b6ee51c342f1a85f285ed6af74a4028d90d04566
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-11-21 17:50:40 +0100

    Trim sur mail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,6 +51,7 @@ export class AppComponent implements OnInit {
   }
 
   validateEmail(email : String) {
+    email=email.trim();
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
